848511051b4b9d923ea1a103c1a07b2b4614a5cd
nanda <nandabahtiar1@gmail.com>
2025-06-30 09:19:08 +0700

    fix:icon

--- src/components/Library/NotFound/NotFound.jsx
+++ src/components/Library/NotFound/NotFound.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import FuzzyText from "../FuzzyText/FuzzyText.jsx";
 import Particles from "../Particles/Particles.jsx";
 import {Link} from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import SplitText from "../SplitText/SplitText.jsx";
 
 const NotFound = () => {
     return (
         // Gunakan div relatif untuk menjadi container bagi elemen absolut di dalamnya
         <div className="relative flex justify-center items-center w-full h-screen bg-white overflow-hidden">
-            
+
             {/* Komponen Partikel sebagai latar belakang */}
             <div className="absolute inset-0 z-0">
                 <Particles
                     particleColors={['#000', '#000']}
-                    particleCount={300}
-                    particleSpread={20}
-                    speed={0.1}
+                    particleCount={500}
+                    particleSpread={30}
+                    speed={0.2}
 
-                    particleBaseSize={200}
+                    particleBaseSize={250}
                     moveParticlesOnHover={false}
                     alphaParticles={true}
                     disableRotation={false}
@@ -25,16 +26,26 @@ const NotFound = () => {
 
             {/* Konten teks di atas partikel */}
             <div className="relative z-10 flex  flex-col justify-center items-center">
-                <FuzzyText 
-                    color="#000" 
-                    fontSize={'clamp(2rem, 10vw, 8rem)'}
-                    fontWeight={800}
-                >
-                    404 Not Found
-                </FuzzyText>
+                <SplitText
+                    text="404 Page Not Found!"
+                    className="text-7xl h-28 font-extrabold text-center text-gray-800 drop-shadow-lg"
+                    delay={200}
+                    duration={1.0}
+                    ease="back.out(1.7)"
+                    splitType="chars"
+                    from={{opacity: 0, y: 40, scale: 0.8}}
+                    to={{opacity: 1, y: 0, scale: 1}}
+                    threshold={0.1}
+                    rootMargin="-100px"
+                    textAlign="center"
+                />
+
                 <br/>
 
-                <Link to="/">      <h1> {'<- Back to Home'}</h1></Link>
+                <Link to="/" className="mt-8 px-8 py-4 bg-primary text-white font-semibold rounded-full shadow-lg hover:bg-green-700 transition duration-300 ease-in-out transform hover:scale-105 flex items-center justify-center space-x-3">
+                    <FaHome className="text-xl" />
+                    <span>Back to Home</span>
+                </Link>
             </div>
 
         </div>
